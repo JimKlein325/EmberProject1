50229c5228553d1eb01f93b145c30922aa81cdc1
Jim Klein <j.mesklein@gmail.com>
2016-08-18 21:51:03 -0700

    add question display and new question creation functionality to index route: question-tile and question-new components.

--- app/components/question-new.js
+++ app/components/question-new.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  formIsVisible: false,
+  actions: {
+    toggleForm() {
+      this.set('formIsVisible', true);
+    },
+    save() {
+      var params = {
+        author: this.get('author'),
+        text: this.get('text'),
+        notes: this.get('notes')
+
+      };
+      this.set('formIsVisible', false);
+      this.sendAction('save', params);
+    }
+  },
 });
