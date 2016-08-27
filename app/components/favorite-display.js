345e44e7ffce19bda76dbbbc3eda296cd43212b0
Jim Klein <j.mesklein@gmail.com>
2016-08-26 17:12:08 -0700

    add favorites display functionality.

---
+++ app/components/favorite-display.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  favoritesList: Ember.inject.service(),
+  answers: Ember.computed('favoritesList.items', function() {
+    return this.get('favoritesList').items;
+  }),
+  answerCount: Ember.computed('favoritesList.items', function() {
+    return this.get('favoritesList').items.length;
+  }),
+
+});
